5a97e1652091b830fed5bf66a47c2b855dd220dc
Natthanicha067 <67030067@kmitl.ac.th>
2024-11-21 18:12:42 +0000

    q

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,3 +1,11 @@
-numbers = [2, 8, 9, 48, 8, 22, -12, 2]
-
-print(numbers)
+import sys
+if len(sys.argv) == 3:
+    keyword = sys.argv[1]
+    search_string = sys.argv[2]
+    count = search_string.count(keyword)
+    if count > 0:
+        print(count)
+    else:
+        print("none")
+else:
+    print("none")
